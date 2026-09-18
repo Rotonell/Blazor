67ffc026719a736431f0882a6ba02c63f949fdd0
Дарья Серова <dashaflay2000@gmail.com>
2026-09-18 18:31:15 +0300

    Students in progress

---
+++ Academy/Models/Human.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academy.Models
+{
+	public class Human
+	{
+		[Required]
+		[StringLength(50, MinimumLength = 2)]
+		[RegularExpression("^[A-ZА-Я][a-zа-я]+$")]
+		public string last_name { get; set; }
+
+		[Required]
+		[StringLength(50, MinimumLength = 2)]
+		public string first_name { get; set; }
+
+		public string? middle_name { get; set; }
+
+		[Required]
+		[DataType(DataType.Date)]
+		public DateOnly birth_date { get; set; }
+
+		[EmailAddress]
+		public string? email { get; set; }
+
+		[Phone]
+		public string? phone { get; set; }
+
+		[Column("photo", TypeName = "IMAGE")]
+		public byte[]? photo { get; set; }
+
+		//			Calculated properties:
+		public string FullName
+		{
+			get => $"{last_name} {first_name} {middle_name}";
+		}
+	}
+}
